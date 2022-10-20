e1d3aaea745b428fab83ce5d9183ab2a25f073af
Mohammed Ashiq S <mrashiq.tvm@gmail.com>
2022-10-20 15:32:42 +0530

    todolistver1

---
+++ date.js
@@ -0,0 +1,39 @@
+
+exports.getDate = getDate;
+function getDate(){
+
+
+
+const today = new Date();
+    
+
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+}
+
+return today.toLocaleDateString("en-US", options)
+
+
+}
+
+
+
+exports.getDay = getDay;
+
+function getDay(){
+
+
+
+    const today = new Date();
+        
+    
+    const options = {
+        weekday: "long"
+    };
+    
+    return today.toLocaleDateString("en-US", options)
+    
+    
+    }
